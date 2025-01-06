0b1234a69479708339221638d2ce7791ea8d78b9
Romain <>
2025-01-06 17:28:23 +0100

    Move example file

--- examples/compare_strategies.py
+++ examples/compare_strategies.py
@@ -1,14 +1,9 @@
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 
-from solar_optimization.core.timeseries import TimeSeriesConfig
-from solar_optimization.core.consumption import BaseConsumption, ConsumptionPeak
-from solar_optimization.core.production import SolarProduction, SolarProductionPeak
+from solar_optimization.core import TimeSeriesConfig, BaseConsumption, ConsumptionPeak, SolarProduction, SolarProductionPeak
 from solar_optimization.devices.cet import CETProperties
-from solar_optimization.strategies.schedule import ScheduledStrategy
-from solar_optimization.strategies.solar_only import SolarOnlyStrategy
-from solar_optimization.strategies.max_solar import MaximizeSolarStrategy
-from solar_optimization.strategies.optimize import OptimizationStrategy
+from solar_optimization.strategies import ScheduledStrategy, SolarOnlyStrategy, MaximizeSolarStrategy, OptimizationStrategy
 from solar_optimization.visualization.plotter import SolarOptimizationVisualizer
 
 def main():
@@ -23,33 +18,17 @@ def main():
 
     # Base consumption setup
     consumption_peaks = [
-        ConsumptionPeak("morning_peak",
-                        2, 
-                       datetime(2024, 1, 1, 7, 0),
-                       datetime(2024, 1, 1, 7, 30)),
-        ConsumptionPeak("evening_peak",
-                        2,
-                       datetime(2024, 1, 1, 19, 0),
-                       datetime(2024, 1, 1, 19, 30)),
-        ConsumptionPeak("Lunch",
-                        1.5,
-                        datetime(2024, 1, 1, 13, 0),
-                        datetime(2024, 1, 1, 13, 30)),
-        ConsumptionPeak("Washing machine",
-                        1.5,
-                        datetime(2024, 1, 1, 10, 30),
-                        datetime(2024, 1, 1, 11, 0))
+        ConsumptionPeak("morning_peak",     2,  datetime(2024, 1, 1, 7, 0), datetime(2024, 1, 1, 7, 30)),
+        ConsumptionPeak("evening_peak",     2,  datetime(2024, 1, 1, 19, 0), datetime(2024, 1, 1, 19, 30)),
+        ConsumptionPeak("Lunch",            1.5,datetime(2024, 1, 1, 13, 0), datetime(2024, 1, 1, 13, 30)),
+        ConsumptionPeak("Washing machine", 1.5, datetime(2024, 1, 1, 10, 30), datetime(2024, 1, 1, 11, 0))
     ]
     base_consumption = BaseConsumption(mean_base=0.5, peaks=consumption_peaks)
 
     # Solar production setup
     solar_peaks = [
-        SolarProductionPeak(1.4,
-                           datetime(2024, 1, 1, 9, 0),
-                           datetime(2024, 1, 1, 11, 30)),
-        SolarProductionPeak(2,
-                           datetime(2024, 1, 1, 11, 0),
-                           datetime(2024, 1, 1, 17, 0))
+        SolarProductionPeak(1.4, datetime(2024, 1, 1, 9, 0), datetime(2024, 1, 1, 11, 30)),
+        SolarProductionPeak(2, datetime(2024, 1, 1, 11, 0), datetime(2024, 1, 1, 17, 0))
     ]
     solar_production = SolarProduction(peaks=solar_peaks)
 
@@ -58,25 +37,19 @@ def main():
     solar_production_data = solar_production.generate(timestamps)
 
     # CET properties
-    cet_properties = CETProperties(
-        power=1,
-        min_duration=timedelta(minutes=15),
-        max_duration=timedelta(hours=4)
-    )
+    cet_properties = CETProperties(power=1, min_duration=timedelta(minutes=15), max_duration=timedelta(hours=4))
 
     # Create and run strategies
     strategies = [
-        ScheduledStrategy("Programmé la nuit", [
-            {"start": datetime(2024, 1, 1, 0, 0),
-             "end": datetime(2024, 1, 1, 2, 0)},
-             {"start": datetime(2024, 1, 1, 22, 0),
-             "end": datetime(2024, 1, 2, 0, 0)}
+        ScheduledStrategy("Night scheduling", [
+            {"start": datetime(2024, 1, 1, 0, 0), "end": datetime(2024, 1, 1, 2, 0)},
+            {"start": datetime(2024, 1, 1, 22, 0),"end": datetime(2024, 1, 2, 0, 0)}
         ]),
-        ScheduledStrategy("Programmé en journée", [
+        ScheduledStrategy("Day scheduling", [
             {"start": datetime(2024, 1, 1, 11, 0),
              "end": datetime(2024, 1, 1, 15, 0)}
         ]),
-        SolarOnlyStrategy("100% Solaire", threshold_start=1.1),
+        SolarOnlyStrategy("100% Solar", threshold_start=1.1),
         MaximizeSolarStrategy("Max. Solar."),
         OptimizationStrategy("Optimiz", threshold=0.5)
     ]
@@ -111,8 +84,6 @@ def main():
     ax_table.axis('off')
     
     # Préparation des données pour le tableau
-    print(results[0][0])
-    print(results[0][1])
     table_data = [
         ['Métrique', 
          f"{results[0][0]}",
@@ -169,19 +140,6 @@ def main():
     table.auto_set_font_size(False)
     table.set_fontsize(9)
     table.scale(1.2, 1.5)
-    # Print comparison of metrics
-    print("\nComparaison des Stratégies:")
-    print("-" * 80)
-    for name, metrics in results:
-        print(f"\nStratégie: {name}")
-        print(f"Import réseau: {metrics.import_reseau:.2f} kWh")
-        print(f"Export réseau: {metrics.export_reseau:.1f} kWh")
-        print(f"Taux autoconsommation: {metrics.taux_autoconsommation:.1f}%")
-        print(f"Coût total: {metrics.cout_total:.2f}€")
-        print(f"Coût moyen/kWh: {metrics.cout_moyen_kwh:.3f}€/kWh")
-        print(f"Coût CET: {metrics.cout_fonctionnement_cet:.2f}€")
-        print(f"CET solar ratio: {metrics.cet_solar_share:.1f}%")
-        print(f"Temps fonctionnement CET: {metrics.temps_fonctionnement_cet:.1f} heures")
 
     plt.show()
 
